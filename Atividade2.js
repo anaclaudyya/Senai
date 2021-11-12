f344837f5350ecc3d796478ffc1d428682047fe3
anaclaudyya <92897363+anaclaudyya@users.noreply.github.com>
2021-11-12 18:57:07 -0300

    Add files via upload

---
+++ Atividade2.js
@@ -0,0 +1,12 @@
+var nome = window.prompt('Digite seu Nome');
+var idade = window.prompt('Digite  a sua Idade'); 
+var esporte = window.prompt('Digite seu Esporte Favorito');
+var diaSemana = window.prompt('Digite dia da Semana predileto');
+var nomeImportante = window.prompt('Digite Nome de Alguém importante');
+var nomeAdmira = window.prompt('Digite Nome de Alguem que vc admira');
+
+var strTeste = "Hoje "+diaSemana+" é um dia Historico Todos os fãs de "+esporte+", estão maravilhados com a proeza "+
+"realizado pelo atleta "+nome+". " + nomeImportante+" não conteve as lagrimas ao comentar em uma entrevista e até "+nomeAdmira+
+" twitou é muito legal ter alguem de " + idade + " Se desenvolvendo tão bem";
+
+alert(strTeste );
